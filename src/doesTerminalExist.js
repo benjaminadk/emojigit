e73ea5b7d570d1489aab8ed774033f1d599b5351
Anand Chowdhary <mail@anandchowdhary.com>
2019-03-15 11:17:44 +0100

    :recycle: Better syntax for doesTerminalExist

--- src/doesTerminalExist.js
+++ src/doesTerminalExist.js
@@ -1,8 +1,5 @@
 const vscode = require('vscode')
 
 module.exports = () => {
-  if (vscode.window.terminals.length === 0) {
-    return false
-  }
-  return true
+  return vscode.window.terminals.length !== 0;
 }
